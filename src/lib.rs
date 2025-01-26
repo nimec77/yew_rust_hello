842214fac7e03e4e7887e6709e75789b988b1306
Dmitry Seloustev <d.seloustev@adguard.com>
2025-01-26 15:30:26 +0700

    Complete part 17: Enum Properties

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,14 @@
-use std::str::FromStr;
-use stylist::ast::Sheet;
 use stylist::yew::styled_component;
-use stylist::Style;
 use yew::prelude::*;
 mod components;
 
-use components::atoms::main_title::MainTitle;
-
-const STYLE_FILE: &str = include_str!("styles/main.css");
+use components::atoms::main_title::{MainTitle, Color};
 
 #[styled_component]
 pub fn App() -> Html {
-    let sheet = Sheet::from_str(STYLE_FILE).unwrap();
-    let stylesheets = Style::new(sheet).unwrap();
-
-    let message = Some("I am a message");
-    let tasks = vec!["Learn Rust", "Learn Yew", "Build cool stuff"];
-
     html! {
-        <div class={stylesheets}>
-            <MainTitle title="Hi there!" />
-            <p>{"The class is my-class"}</p>
-
-            if let Some(message) = message {
-                <p>{message}</p>
-            }
-
-            <ul>
-                { list_to_html(tasks) }
-            </ul>
+        <div>
+            <MainTitle title="Hi there!" color={Color::Ok} />
         </div>
     }
-}
-
-fn list_to_html(list: Vec<&str>) -> Vec<Html> {
-    list.iter().map(|item| html! { <li>{item}</li> }).collect()
 }
