6f57e9f11f533f1fd43c42705bad5118177476e2
Dmitry Seloustev <d.seloustev@adguard.com>
2025-02-01 16:53:56 +0700

    Complete part 22: Multiple use_state hooks in the same component

--- src/components/molecules/custom_form.rs
+++ src/components/molecules/custom_form.rs
@@ -5,15 +5,22 @@ use yew::prelude::*;
 #[function_component(CustomForm)]
 pub fn custom_form() -> Html {
     let username_state = use_state(|| "no username set".to_owned());
+    let button_count_state = use_state(|| 0_u32);
     let cloned_username_state = username_state.clone();
     let username_change = Callback::from(move |username: String| {
         cloned_username_state.set(username);
     });
+    let cloned_button_count_state = button_count_state.clone();
+    let button_clicked = Callback::from(move |_| {
+        let count = *cloned_button_count_state;
+        cloned_button_count_state.set(count + 1);
+    });
     html! {
-        <form>
+        <div>
             <TextInput name="username" handle_onchange={username_change} />
-            <CustomButton label="Submit" />
+            <CustomButton label="Submit" onclick={button_clicked} />
             <p>{"Username: "}{&*username_state}</p>
-        </form>
+            <p>{"Button clicked count: "}{*button_count_state}</p>
+        </div>
     }
 }
