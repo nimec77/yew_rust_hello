1741e5b565f32b60a1bd3b8ac59a48c5b693ec47
Dmitry Seloustev <d.seloustev@adguard.com>
2025-02-02 18:02:01 +0700

    Complete part 29: Internal links

--- src/components/pages/home.rs
+++ src/components/pages/home.rs
@@ -1,10 +1,15 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+use crate::router::Route;
 
 #[function_component]
 pub fn Home() -> Html {
     html! {
         <div>
             <h1>{"Home"}</h1>
+            <div>
+                <Link<Route> to={Route::Hello}> {"To Hello"} </Link<Route>>
+            </div>
         </div>
     }
 }
